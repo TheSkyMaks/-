bcc4782299e93884d73b12e167cb320d9f0f8af5
theskymaks <theskymaks@gmail.com>
2020-07-11 16:45:24 +0300

    Add project files.

---
+++ MainFile.cs
@@ -0,0 +1,11 @@
+﻿namespace ZNO_ukr
+{
+    internal static class MainFile
+    {
+        private static void Main(string[] args)
+        {
+            var start = new MainMenu();
+            start.Start();
+        }
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ZNO_ukr
+{
+    public class MainMenu
+    {
+        public void Start()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter number: (1)");
+                int.TryParse(Console.ReadLine(), out int key);
+                switch (key)
+                {
+                    case 1:
+                        var startNagolosi = new Nagolosi();
+                        startNagolosi.Start();
+                        return;
+
+                    default:
+                        Console.WriteLine("Error: Invalid key, try again. Press Enter to continue...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
